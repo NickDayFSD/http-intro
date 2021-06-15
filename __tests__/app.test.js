5389c4012f867dbe647863295951fde3185f3333
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-15 10:48:48 -0700

    red test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,4 +13,21 @@ describe('app routes', () => {
 
     expect(res.text).toEqual('WAFFLES!');
   });
+
+  it('takes you to the red page', async() => {
+    const res = await request(app).get('/red');
+
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> red </h1>
+</body>
+</html>`);
+  });
 });
