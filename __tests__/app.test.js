7ed09a6c0212be37c41f11da13f1685b5f25c6d7
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-15 10:20:49 -0700

    wrote first route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('finds root and says "hi"', async() => {
+    const res = await request(app).get('/');
 
+    expect(res.text).toEqual('hi');
+  });
 });
