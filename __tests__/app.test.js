861ae148613709f8753c1e32938b91dd9f697d2d
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-17 09:50:47 -0700

    created index route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -70,4 +70,24 @@ describe('app routes', () => {
 
     expect(res.text).toEqual('Not Found');
   });
+
+  it('routes to html and serves public/index.html', async() => {
+    const res = await request(app).get('/index');
+
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Weather</title>
+</head>
+
+<body>
+  <h1>It's getting hot out there, Index!</h1>
+</body>
+
+</html>`);
+  });
 });
